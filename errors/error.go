c905a67b640146c21f5f6aa772d531b5bb8f5f17
Aniket Chavan <chavaniket360@protonmail.com>
2020-05-28 22:30:17 +1000

    error.go: Removed need for passing in a map, instead creates map within the AxiomsError function itself

--- errors/error.go
+++ errors/error.go
@@ -1,8 +1,12 @@
 package errors
 
 // AxiomsError is a custom error
-func AxiomsError(text map[string]string, code string) error {
-	return &errorString{text, code}
+func AxiomsError(err string, description string, code string) error {
+	var errObj = map[string]string{
+		"error":             err,
+		"error_description": description,
+	}
+	return &errorString{errObj, code}
 }
 
 type errorString struct {
