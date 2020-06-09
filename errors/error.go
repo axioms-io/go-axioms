879994e7de74d780c27cab0da0884b229648c26a
Aniket Chavan <chavaniket360@protonmail.com>
2020-06-10 02:49:49 +1000

    error.go: Added ErrorController and functions for error routes

--- errors/error.go
+++ errors/error.go
@@ -1,19 +1,61 @@
 package errors
 
+import "github.com/astaxie/beego"
+
+// ErrorController namespace controller
+type ErrorController struct {
+	beego.Controller
+}
+
+// Error401 handler
+func (e *ErrorController) Error401() {
+	var description string = e.Ctx.Input.Param(":all")
+	e.Ctx.ResponseWriter.WriteHeader(401)
+	e.Data["json"] = AxiomsError(
+		"unauthorized_access",
+		description,
+		401,
+	)
+	e.ServeJSON()
+}
+
+// Error403 handler
+func (e *ErrorController) Error403() {
+	e.Ctx.ResponseWriter.WriteHeader(403)
+	e.Data["json"] = AxiomsError(
+		"insufficient_permission",
+		"Insufficient role, scope or permission",
+		403,
+	)
+	e.ServeJSON()
+}
+
+// Error404 handler
+// func (e *ErrorController) Error404() {
+// 	e.Ctx.ResponseWriter.WriteHeader(404)
+// 	e.Data["json"] = AxiomsError(
+// 		"page_not_found",
+// 		"This route does not exist",
+// 		404,
+// 	)
+// 	e.ServeJSON()
+// }
+
 // AxiomsError is a custom error
 func AxiomsError(err string, description string, code int) error {
 	var errObj = map[string]string{
 		"error":             err,
 		"error_description": description,
 	}
-	return &errorString{errObj, code}
+	return &errorResponse{errObj, code}
 }
 
-type errorString struct {
-	name map[string]string
-	code int
+// ErrorResponse exported
+type errorResponse struct {
+	Name map[string]string `json:"errmsg"`
+	Code int               `json:"errcode"`
 }
 
-func (e *errorString) Error() string {
-	return e.name["error"]
+func (e *errorResponse) Error() string {
+	return e.Name["error"]
 }
