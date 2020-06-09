7c94e267f7c0e9855fc1d6073883f3f4cd4699a8
Aniket Chavan <chavaniket360@protonmail.com>
2020-06-10 02:50:43 +1000

    router.go: Fixed permission filter, added error routing

--- routers/router.go
+++ routers/router.go
@@ -9,14 +9,42 @@ package routers
 
 import (
 	"go-axioms/controllers"
+	axerr "go-axioms/errors"
+	"go-axioms/filters"
 
 	"github.com/astaxie/beego"
 )
 
+// func methodSwitcher(in string) {
+// 	switch method := in; method {
+// 	case "GET":
+// 		perm := []string{"sample:read"}
+// 		return filters.HasRequiredPermissions(perm)
+// 	case "POST":
+// 		perm := []string{"sample:create"}
+// 		return filters.HasRequiredPermissions(perm)
+// 	case "PUT":
+// 		perm := []string{"sample:update"}
+// 		return filters.HasRequiredPermissions(perm)
+// 	default:
+// 		perm := []string{"sample:unknown"}
+// 		return filters.HasRequiredPermissions(perm)
+// 	}
+// }
+
 func init() {
 	beego.Router("/", &controllers.IndexController{})
 	beego.Router("/permission", &controllers.PermissionController{})
+	beego.InsertFilter(
+		"/permission",
+		beego.BeforeExec,
+		filters.HasRequiredPermissions([]string{"sample:read"}),
+	)
+
 	beego.Router("/private", &controllers.PrivateController{})
 	beego.Router("/public", &controllers.PublicController{})
 	beego.Router("/role", &controllers.RoleController{})
+
+	beego.Router("/401/:all", &axerr.ErrorController{}, "GET:Error401")
+	beego.Router("/403", &axerr.ErrorController{}, "GET:Error403")
 }
